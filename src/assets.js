1dc8d14253af824e16dd2858a5a9103845713aa7
skJaganFalconicx <117151077+skJaganFalconicx@users.noreply.github.com>
2022-12-22 14:23:38 +0530

    Add files via upload

--- src/assets.js
+++ src/assets.js
@@ -26,8 +26,8 @@ export default {
     pano2: { url: 'Ocean.jpg', options: { encoding: THREE.sRGBEncoding, flipY: true} },
     pano3: { url: 'Tea_plantation.jpg', options: { encoding: THREE.sRGBEncoding, flipY: true} },
     pano4: { url: 'Mine.jpg', options: { encoding: THREE.sRGBEncoding, flipY: true} },
-    pano5: { url: 'Museum.basis', options: { encoding: THREE.sRGBEncoding, flipY: true} },
-    pano6: { url: 'Ancient_rome.basis', options: { encoding: THREE.sRGBEncoding, flipY: false} },
+    pano5: { url: 'Museum.jpg', options: { encoding: THREE.sRGBEncoding, flipY: true} },
+    pano6: { url: 'Ancient_rome.jpg', options: { encoding: THREE.sRGBEncoding, flipY: false} },
 
   // graffiti
   spray_model: { url: 'paintingBrush.glb' },
